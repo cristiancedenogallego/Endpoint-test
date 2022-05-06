0159a0451e0e491afe83e0a22c644213546f5455
cristian cedeno <ccedeno@inmov.com>
2022-05-06 07:24:35 -0500

    Print folder names alphabetically

--- src/statements/ListStatement.ts
+++ src/statements/ListStatement.ts
@@ -5,13 +5,23 @@ export class ListStatement implements Statement {
   constructor(private root: TreeFolder) {
   }
 
+  compareNodeNames(a: TreeFolder, b: TreeFolder): number {
+    if (a.name < b.name) {
+      return -1;
+    }
+    if (a.name < b.name) {
+      return 1;
+    }
+    return 0;
+  }
+
   printNode(root: TreeFolder, level = 0) {
     // This for the empty root
     if (root.name) {
       console.log(`${'-'.repeat(level)}${root.name}`);
     }
 
-    root?.children?.forEach((ch) => {
+    root?.children?.sort(this.compareNodeNames).forEach((ch) => {
       this.printNode(ch, level + 1);
     })
   }
